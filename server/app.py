fa90dc1953b3097d4de79c85d4fa98a7d8d6fd66
baimet <isaacbaimetyatich@gmail.com>
2024-10-11 04:30:01 +0300

    CRUD

--- server/app.py
+++ server/app.py
@@ -47,7 +47,28 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
-
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+            if 'is_in_stock' in data:
+                plant.is_in_stock = data['is_in_stock']
+            db.session.commit()
+            
+            response_dict = plant.to_dict()
+            return make_response(jsonify(response_dict), 200)
+        else:
+            return make_response({'error': 'Plant not found'}, 404)
+        
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)
+        else:
+            return make_response({'error': 'Plant not found'}, 404)
+        
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
